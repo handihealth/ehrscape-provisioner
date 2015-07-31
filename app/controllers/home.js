496e12f7eec2e8561edacdbe2c649eeb73f48afe
David Thompson <davethompson21@gmail.com>
2015-07-31 11:57:44 +0100

    Refactor action http code to be more modular

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -14,12 +14,18 @@ angular.module('ehrscapeProvisioner.home', ['ngRoute', 'ngQueue'])
 
   totalActionCount = 3;
   completeActionCount = 0;
-
   startTime = 0;
   endTime = 0;
 
   prepareActionList = function(Action) {
     var actionList = [];
+    actionList.push(new Action({
+      id: 'LOGIN',
+      name: 'Login',
+      urlExtension: 'session',
+      requestMethod: 'POST',
+      includeSessionHeader: false
+    }));
     actionList.push(new Action({
       id: 'CREATE_PATIENT',
       name: 'Create patient',
@@ -34,97 +40,96 @@ angular.module('ehrscapeProvisioner.home', ['ngRoute', 'ngQueue'])
       urlExtension: 'ehr',
       requestMethod: 'POST'
     }));
-
     return actionList;
   };
 
-  setLoginResponseData = function(loginAction, result) {
-    loginAction.endTime = Date.now();
-    loginAction.status = result.status;
-    loginAction.responseCode = result.responseCode;
-    loginAction.responseBody = JSON.stringify(result.responseData, null, 2);
+  setResponseData = function(actionItem, result) {
+    actionItem.endTime = Date.now();
+    actionItem.status = result.status;
+    actionItem.responseCode = result.responseCode;
+    actionItem.responseBody = JSON.stringify(result.responseData, null, 2);
     completeActionCount++;
   }
 
-  afterLoginSuccess = function(loginAction, result) {
-    setLoginResponseData(loginAction, result);
-    $rootScope.ehrscapeConfig.sessionId = result.responseData.sessionId;
-    var queue = $queueFactory($scope.actionList.length);
+  prePerformHttpRequest = function(currAction) {
+    if (currAction.id === 'LOGIN') {
+      currAction.setUrlParameters(
+        [
+          {name: 'username', config: true},
+          {name: 'password', config: true}
+        ]
+      );
+    }
+    if (currAction.id === 'CREATE_EHR') {
+      currAction.setUrlParameters(
+        [
+          {name: 'subjectId', config: true},
+          {name: 'subjectNamespace', config: true},
+          {name: 'commiterName', config: true},
+        ]
+      );
+    }
+  }
 
-    for (var i = 0; i < $scope.actionList.length; i++) {
+  postPerformHttpRequest = function(currAction, result) {
+    if (currAction.id === 'CREATE_PATIENT') {
+      var subjectId = result.responseData.meta.href;
+      subjectId = subjectId.substr(subjectId.lastIndexOf('/')+1);
+      $rootScope.ehrscapeConfig.subjectId = subjectId;
+    }
+  }
+
+  queueFollowingActionHttpRequests = function() {
+    var queue = $queueFactory($scope.actionList.length-1);
+    for (var i = 1; i < $scope.actionList.length; i++) {
       queue.enqueue(function () {
         var currAction = $scope.actionList[i];
-
-        if (currAction.id === 'CREATE_EHR') {
-          currAction.setUrlParameters(
-            [
-              {name: 'subjectId', config: true},
-              {name: 'subjectNamespace', config: true},
-              {name: 'commiterName', config: true},
-            ]
-          );
-        }
-
+        prePerformHttpRequest(currAction);
         return currAction.performHttpRequest(function(result) {
-          currAction.endTime = Date.now();
-          currAction.status = result.status;
-          currAction.responseCode = result.responseCode;
-          currAction.responseBody = JSON.stringify(result.responseData, null, 2);
-          completeActionCount++;
-
-          if (currAction.id === 'CREATE_PATIENT') {
-            var subjectId = result.responseData.meta.href;
-            subjectId = subjectId.substr(subjectId.lastIndexOf('/')+1);
-            $rootScope.ehrscapeConfig.subjectId = subjectId;
-          }
-
+          setResponseData(currAction, result);
+          postPerformHttpRequest(currAction, result);
+        }, function(result) {
+          setResponseData(currAction, result);
+          //TODO: if any requests fail then probably don't do the rest.
         });
       });
     }
-  };
+  }
 
-  afterLoginFailure = function (loginAction, result) {
-    setLoginResponseData(loginAction, result);
-  };
-
-  $scope.start = function() {
-
-    completeActionCount = 0;
-
-    if ($rootScope.ehrscapeConfig.username.length === 0 || $rootScope.ehrscapeConfig.password.length === 0) {
-      swal('Error', 'Please enter username and password');
-      return;
-    }
-
-    $scope.loginAction.setUrlParameters(
+  processActionHttpRequests = function() {
+    var loginAction = $scope.actionList[0];
+    loginAction.setUrlParameters(
       [
         {name: 'username', config: true},
         {name: 'password', config: true}
       ]
     );
-    var loginAction = $scope.loginAction;
-    loginAction.performHttpRequest(
-      function(result) {
-        afterLoginSuccess(loginAction, result)
-      },
-      function(result) {
-        afterLoginFailure(loginAction, result)
+    loginAction.performHttpRequest(function(result) {
+      setResponseData(loginAction, result);
+      $rootScope.ehrscapeConfig.sessionId = result.responseData.sessionId;
+      queueFollowingActionHttpRequests();
+    }, function(result) {
+      setResponseData(loginAction, result);
+      if ($rootScope.ehrscapeConfig.sessionId === '') {
+        swal('Error', 'Authentication failed, please check the username and password.');
+        return;
       }
-    );
+    });
+  };
 
+  $scope.start = function() {
+    completeActionCount = 0;
+    if ($rootScope.ehrscapeConfig.username.length === 0 || $rootScope.ehrscapeConfig.password.length === 0) {
+      swal('Error', 'Please enter username and password');
+      return;
+    }
+    processActionHttpRequests();
   };
 
   $scope.reset = function() {
     $rootScope.ehrscapeConfig = ehrscapeConfig;
-    $scope.loginAction = new Action({
-      id: 'LOGIN',
-      name: 'Login',
-      urlExtension: 'session',
-      requestMethod: 'POST',
-      includeSessionHeader: false
-    });
-    $scope.actionItem = $scope.loginAction;
     $scope.actionList = prepareActionList(Action);
+    completeActionCount = 0;
   };
 
   $scope.getPercentComplete = function() {
