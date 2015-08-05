0b306395884c750c5b96807b17de98ceb812e696
David Thompson <davethompson21@gmail.com>
2015-08-05 12:10:11 +0100

    Using gulp-karma to run tests locally, leaving npm test for CI process

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,33 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var copy = require('gulp-copy');
+var karma = require('gulp-karma');
+
+var testFiles = [
+  './app/bower_components/angular/angular.js',
+  './app/bower_components/angular-route/angular-route.js',
+  './app/bower_components/angular-mocks/angular-mocks.js',
+  './app/bower_components/ngQueue/ngQueue.js',
+  './app/bower_components/sweetalert/dist/sweetalert.min.js',
+  './app/app.js',
+  './app/controllers/*.js',
+  './app/directives/*.js',
+  './app/data/*.js',
+  './app/models/*.js',
+  './test/controllers/*.js',
+  './test/models/*.js',
+];
+
+gulp.task('test', function() {
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      throw err;
+    });
+});
 
 gulp.task('default', function() {
   gulp.start('combine-angular', 'sass');
@@ -39,7 +66,20 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
+gulp.task('watch-test', function() {
+  gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./assets/stylesheets/*.scss', ['sass']);
   gulp.watch(['./app/*.js', './app/*/*.js'], ['combine-angular']);
+  gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
 });
